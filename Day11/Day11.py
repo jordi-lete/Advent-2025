06ab3b081932879a6fa95d179c3fb75a67ce1889
jordi-lete <jordismith20@hotmail.com>
2025-12-22 19:29:50 +0000

    added description

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,6 +1,13 @@
 ''' 
 =============== Advent Day 11 ===============
 
+Each line gives the name of a device followed by a list of the devices to which its outputs are attached. 
+So, bbb: ddd eee means that device bbb has two outputs, one leading to device ddd and the other leading to device eee.
+The Elves are pretty sure that the issue isn't due to any specific device, but rather that the issue is triggered by data following some specific path through the devices. 
+Data only ever flows from a device through its outputs; it can't flow backwards.
+
+To help the Elves figure out which path is causing the issue, they need you to find every path from you to out.
+How many different paths lead from you to out?
 
 ============================================
 '''
@@ -43,27 +50,14 @@ class Solution:
         
         return graph
 
-    def find_all_paths(self, graph, start, end, path=[]):
-        path = path + [start]
-
-        if start == end:
-            return [path]
-        
-        if start not in graph:
-            return []
-        
-        paths = []
-        for node in graph[start]:
-            if node not in path:
-                newpaths = self.find_all_paths(graph, node, end, path)
-                for newpath in newpaths:
-                    paths.append(newpath)
-                    
-        return paths
+    def count_paths():
+        return 1
 
     def day_11(self, input):
         graph = self.process_input(input)
-        paths = self.find_all_paths(graph, 'you', 'out')
+        start = 'svr'
+        visited = {tuple(start)}
+        paths = self.find_all_paths(graph, start, 'out', visited)
         return len(paths)
 
 
